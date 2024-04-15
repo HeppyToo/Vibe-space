6554e7889e4888af8056081ce0977f5edcafbaa9
HeppyTwo <smykdev15@gmail.com>
2024-04-15 15:20:51 +0300

    add function register

--- action/register.ts
+++ action/register.ts
@@ -1,15 +1,35 @@
 "use server"
 
 import * as z from "zod"
+import bcrypt from "bcryptjs"
 
 import {RegisterSchema} from "@/schemas";
+import {db} from "@/lib/db";
+import {getUserByEmail} from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.parse(values)
+    const validatedFields = RegisterSchema.safeParse(values)
 
-    if(!validatedFields) {
+    if(!validatedFields.success) {
         return { error: "Invalid fields!"}
     }
 
-    return { success: "Email send!"}
+    const {email, password, name} = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser) {
+        return { error: "User already exists!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    return { success: "User created!"}
 }
--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -11,7 +11,7 @@ const font = Poppins({
 export default function Page() {
     return (
         <main
-            className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            className="flex h-full flex-col items-center justify-center">
             <div className="space-y-6 text-center">
                 <h1 className={cn(
                     "text-6xl font-semibold text-white drop-shadow-md",
