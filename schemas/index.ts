66df0b0209a2631843a1e2166dde4ee0939c8fca
HeppyTwo <smykdev15@gmail.com>
2024-04-26 21:44:40 +0300

    add resend password form

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,9 @@
 import * as z from 'zod';
 
+export const  NewPasswordSchema = z.object({
+    password: z.string().min(6, {message: 'Password is required'}),
+});
+
 export const  ResetSchema = z.object({
     email: z.string().email({
         message: 'Email is required'
